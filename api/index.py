4d5615fd427c909e7ac26e35c760f5176e57ab95
Dennis Fiallo <dfiallo35@gmail.com>
2024-05-21 23:10:43 -0400

    add middleware

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
@@ -6,3 +7,11 @@ app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 @app.get("/api/health")
 async def health():
     return "online"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
