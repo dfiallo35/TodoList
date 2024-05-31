801a11ebe67f91c02cbef12e4cdb9f0d5f1c8a2a
Dennis Fiallo <dfiallo35@gmail.com>
2024-05-31 13:01:32 -0400

    final backend frontend integration

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+)
 
-@app.get("/")
+
+@app.get("/api/")
 async def read_root():
     return {"message": "Hello World"}
