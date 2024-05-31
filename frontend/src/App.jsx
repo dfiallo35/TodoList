0558dbce7c07fc213784d682f05f6a4b1e93dd17
Dennis Fiallo <dfiallo35@gmail.com>
2024-05-31 19:14:05 -0400

    setup fronted and backend connections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import Register from "./pages/Register";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-
-function Logout() {
-  localStorage.clear();
-  return <Navigate to="/login" />;
-}
-
-function RegisterAndLogout() {
-  localStorage.clear();
-  return <Register />;
-}
 
 
 function App() {
@@ -24,17 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="*" element={<NotFound />}/>
-        {/* <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/logout" element={<Logout />}/>
-        <Route path="/register" element={<RegisterAndLogout />}/>*/}
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../api';
 
+
 function Home() {
     const [home, setHome] = useState('');
 
