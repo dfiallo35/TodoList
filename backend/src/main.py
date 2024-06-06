d672e17146e5507f6716e1e99268113721e3cfdf
lauolivia <guerralauren44@gmail.com>
2024-06-05 22:56:03 -0400

    cors-middelware

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,7 +5,7 @@ from random import randrange
 from fastapi import FastAPI
 from fastapi import status
 from fastapi import Request
-# from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import ToDo
 from models import ToDoList
@@ -29,6 +29,21 @@ async def add_process_time_header(request: Request, call_next):
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
     return response
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get(
